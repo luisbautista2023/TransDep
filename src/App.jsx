6c142f30ecb9c85544cd1c7b435913af661a9b93
luisbautista2023 <luisbautistareyes2023@gmail.com>
2024-06-07 09:58:56 -0500

    Creacion de Home
    Actualizacion home

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
+import { Route, Routes } from "react-router-dom";
+
+import Nosotros from "./pages/Nosotros";
+import Home from "./pages/Home";
+import Contacto from "./pages/Contacto";
+import NuestrosServicios from "./pages/NuestrosServicios";
+import Layout from "./components/Layout";
+
+import './styles/styles.scss'
+
+
 
 function App() {
-
   return (
     <>
-  
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/nosotros" element={<Nosotros />} />
+          <Route path="/contacto" element={<Contacto />} />
+          <Route path="/nuestro servicio" element={<NuestrosServicios />} /> 
+          <Route path="*" element={<h1> pagina 404 </h1>} />
+        </Route>
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
