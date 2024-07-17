601bb532ed48053e67fd25ef56ba0f7adf22cfbf
luisbautista2023 <luisbautistareyes2023@gmail.com>
2024-07-16 23:14:58 -0500

    creación de pagina contacto

--- src/pages/NuestrosServicios/NuestrosServicios.jsx
+++ src/pages/NuestrosServicios/NuestrosServicios.jsx
@@ -5,9 +5,8 @@ const NuestrosServicios = () => {
         <span className="fondo-curvo"></span>
         <h1 className="n-servi-titulo">NUESTROS SERVICIOS</h1>
         <div className="n-servi-cards">
-
           <div className="n-servi-card">
-            <h3 className="card-texto">Transporte Empresarial.</h3>
+            <h3 className="card-texto">Transporte Empresarial</h3>
             <img
               className="card-fondo"
               src="https://images.pexels.com/photos/977213/pexels-photo-977213.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
@@ -16,18 +15,30 @@ const NuestrosServicios = () => {
           </div>
 
           <div className="n-servi-card">
-            <h3 className="card-texto"> Transporte Escolar.</h3>
-            <img className="card-fondo" src="https://cdn.pixabay.com/photo/2014/06/04/16/43/passenger-362169_1280.jpg" alt="escolar" />
+            <h3 className="card-texto"> Transporte Escolar</h3>
+            <img
+              className="card-fondo"
+              src="https://cdn.pixabay.com/photo/2014/06/04/16/43/passenger-362169_1280.jpg"
+              alt="escolar"
+            />
           </div>
 
           <div className="n-servi-card">
-            <h3 className="card-texto"> Transporte Usuarios de la Salud.</h3>
-            <img  className="card-fondo" src="https://cdn.pixabay.com/photo/2019/04/09/10/40/dlrg-4114112_1280.jpg" alt="escolar" />
+            <h3 className="card-texto"> Transporte Usuarios de la Salud</h3>
+            <img
+              className="card-fondo"
+              src="https://cdn.pixabay.com/photo/2019/04/09/10/40/dlrg-4114112_1280.jpg"
+              alt="escolar"
+            />
           </div>
 
           <div className="n-servi-card">
-            <h3 className="card-texto">Transporte Turístico.</h3>
-            <img  className="card-fondo" src="https://cdn.pixabay.com/photo/2019/07/20/14/46/bicycle-4350883_1280.jpg" alt="escolar" />
+            <h3 className="card-texto">Transporte Turístico</h3>
+            <img
+              className="card-fondo"
+              src="https://images.pexels.com/photos/9354752/pexels-photo-9354752.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="escolar"
+            />
           </div>
         </div>
       </div>
